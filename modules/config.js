2b161da95c3d3fc345b1aa98aff198f541321513
MunoChan <munouchan@protonmail.com>
2018-12-23 16:09:09 +0900

    JsDoc追加

--- modules/config.js
+++ modules/config.js
@@ -5,11 +5,18 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 
+/**設定ファイルのパス */
 const CONFIG_FILE_PATH = "./config.yml";
+/**デフォルト設定ファイルのパス */
 const DEFAULT_CONFIG_FILE_PATH = "./res/config.yml";
 
 //*==========
 
+/**
+ * 設定ファイルが存在するか調べる
+ * @returns {boolean} true:存在 false:存在しない
+ * @author MunoChan
+ */
 function existsConfigFile()
 {
     try
@@ -23,11 +30,20 @@ function existsConfigFile()
     return true;
 }
 
+/**
+ * デフォルトの設定ファイルを新規作成
+ * @author MunoChan
+ */
 function createConfigFile()
 {
     fs.copyFileSync(DEFAULT_CONFIG_FILE_PATH, CONFIG_FILE_PATH);
 }
 
+/**
+ * 設定ファイルを読み込み
+ * @returns {Object} 設定マップ
+ * @author MunoChan
+ */
 function loadConfigFile()
 {
     return yaml.safeLoad(fs.readFileSync(CONFIG_FILE_PATH));
