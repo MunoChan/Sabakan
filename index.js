393452ccef2f85b3a3162b4f7e3596d2af637aa2
MunoChan <munouchan@protonmail.com>
2018-12-23 16:15:35 +0900

    configモジュールで設定読み込み

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//*=====メインモジュール=====
+
+const config = require("./modules/config");
+
+//*==========
+
+try
+{
+    if(config.existsConfigFile())
+    {
+        const conf = config.loadConfigFile();
+        if(checkConfig(conf))
+        {
+            console.log(conf);
+        }
+        else
+        {
+            // 設定に不備あり
+            console.log("設定に不備があります。");
+            console.log("token: '" + conf["token"] + "'");
+            console.log("prefix: '" + conf["prefix"] + "'");
+        }
+    }
+    else
+    {
+        // 設定ファイルが存在しない場合は作成
+        config.createConfigFile();
+        console.log("設定ファイルを新規作成しました。");
+    }
+}
+catch(e)
+{
+    console.log(e)
+}
+
+/**
+ * 設定チェック
+ * @param {Object} conf
+ * @returns {boolean} true:不備なし false:不備あり
+ * @author MunoChan
+ */
+function checkConfig(conf)
+{
+    return conf != undefined && conf != null &&
+            conf["token"] != undefined && conf["token"] != null &&
+            conf["prefix"] != undefined && conf["prefix"] != null &&
+            conf["token"].length > 0 && conf["prefix"].length > 0;
+}
